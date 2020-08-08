383dee352d4eb8f16c7e8660fd60700bfa0505e1
DEEPmagicman <47936160+DEEPmagicman@users.noreply.github.com>
2020-08-08 15:28:14 +0530

    Add files via upload

--- Website/functions.js
+++ Website/functions.js
@@ -6,9 +6,18 @@ const dob = document.getElementById("dob");
 const city = document.getElementById("city");
 const state = document.getElementById("state");
 const bg = document.getElementById("bg");
-const bdonor = document.getElementById("donate");
-const platdonor = document.getElementById("donate");
-const plasdonor = document.getElementById("donate");
+const bdonor = document.querySelector('.checkbox').checked;
+const platdonor = document.getElementById("platdonor").checked;
+const plasdonor = document.getElementById("plasdonor").checked;
+
+var currentDate = new Date();
+var date = currentDate.getDate();
+var month = currentDate.getMonth(); 
+var year = currentDate.getFullYear();
+var time = currentDate.getHours();
+var min = currentDate.getMinutes();
+
+var dateString = date + "-" +(month + 1) + "-" + year + " at" + time + ":" + min;
 
 const button1 = document.getElementById("button1");
 
@@ -19,15 +28,17 @@ const usersCollection = database.collection('users');
 button1.addEventListener("click", (e) => {
   e.preventDefault();
   usersCollection.doc(email.value).set({
-      email: email.value,
-      password: pass.value,
-      phone: phone.value,
-      dob:dob.value,
-      city: city.value,
-      state: state.value,
+      "Name":username.value,
+      "Email Id": email.value,
+      "Password": pass.value,
+      "Phone Number": phone.value,
+      "Date Of Birth":dob.value,
+      "City": city.value,
+      "State": state.value,
       "Blood Group": bg.value,
-      blood_d: bdonor.value,
-      Plate_d:platdonor.value,
-      Plasma_d: plasdonor.value
+      "Blood Donor": bdonor,
+      "Platelets Donor":platdonor,
+      "Plasma Donor": plasdonor,
+      "timestamp": dateString
   });
 });
